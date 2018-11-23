a3e899aef894de95dc4649ce8edba8fd10cde4b0
Maxime Blanchon <maximeblanchon91490@gmail.com>
2018-11-23 16:39:16 +0100

    Transférer les fichiers vers 'IHM'
    "ajout de l'IHM"

---
+++ IHM/OpenImage.java
@@ -0,0 +1,38 @@
+import java.awt.*;  
+import java.awt.event.*;  
+import javax.swing.*;  
+import javax.swing.event.*;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class OpenImage extends JLabel{
+
+	String baseimg = new String("img/noimg.png");
+
+	public OpenImage(int width, int height, String imgpath){
+		File path = new File(imgpath);
+
+		if(path.exists()){
+			displayImage(width, height, imgpath, path);
+		}else{
+			File newpath = new File(baseimg);
+			displayImage(width, height, baseimg, newpath);
+		}
+	}
+
+	void displayImage(int width, int height, String imgpath, File file){
+
+		try{
+			BufferedImage myPicture = ImageIO.read(file);
+			ImageIcon imgicon = new ImageIcon(myPicture);
+			ImageIcon icon = new ImageIcon(new ImageIcon(imgpath).getImage().getScaledInstance(width, height, Image.SCALE_DEFAULT));
+			this.setIcon(icon);
+
+		}catch(IOException e){
+			this.setText("Erreur lors du chargement de l'image");
+		}
+	}
+}  
+
