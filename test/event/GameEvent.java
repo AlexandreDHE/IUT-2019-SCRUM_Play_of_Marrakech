ab6e8aef6102fccd3dd1e0bcf13d8e96cfc5cf32
djadi <djadi@localhost>
2018-11-29 21:07:29 +0100

    Transférer les fichiers vers 'test/event'

---
+++ test/event/GameEvent.java
@@ -0,0 +1,22 @@
+package event;
+
+public class GameEvent implements Event
+{
+	protected int oldState;
+	protected int newState;
+
+	public GameEvent(int oldState, int newState)
+	{
+		this.oldState = oldState;
+		this.newState = newState;
+	}
+
+	public int getOldState()
+	{
+		return this.oldState;
+	}
+	public int getNewState()
+	{
+		return this.newState;
+	}
+}
