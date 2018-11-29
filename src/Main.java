1a6782b3651f8f3dc39fe39f24ce85e99a618dae
Maxime Blanchon <maximeblanchon91490@gmail.com>
2018-11-29 16:27:45 +0100

    Transférer les fichiers vers 'src'

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
+public class Main{
+
+	public static void main(String[] args) {
+
+		Menu menu = new Menu();
+		menu.setVisible(true);
 
 
-public class Main {
-    public static void main(String[] args) {
+	}
 
-        PlateauJeu plateau = new PlateauJeu();
-
-
-    }
 }
