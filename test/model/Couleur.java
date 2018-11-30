88a820d482600fd1b3d4c44dec8bdcb05cc207a8
djadi <djadi@localhost>
2018-11-30 13:42:25 +0100

    Supprimer 'test/model/Couleur.java'

--- test/model/Couleur.java
+++
@@ -1,8 +0,0 @@
-package model;
-public class Couleur
-{
-	public static final int JOUEUR1 = 0;
-	public static final int JOUEUR2 = 1;
-	public static final int JOUEUR3 = 2;
-	public static final int JOUEUR4 = 3;
-}
