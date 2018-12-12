2af595ed1d8a27c76944b91e718f09a8a5c0cb60
Boumallouk <boumallo@salle235-15.arda>
2018-12-12 15:24:35 +0000

    Modif du dé

--- src/model/De.java
+++ src/model/De.java
@@ -1,28 +1,39 @@
 package model;
 import java.util.Random;
 
+import model.de.*;
+
 public class De
 {
 	protected static De de = null;
 	protected  static Random aleatoire;
+
+	protected int[] valeurs;
 	
-	private De()
+	private De(DeManager manager)
 	{
 		aleatoire = new Random();
+
+		valeurs = manager.face(6);
+		for(int i = 0; i< valeurs.length; i++)
+		{
+			System.out.println(valeurs[i] + " ");
+		}
 	}
 
 	public static De getDe()
 	{
 		if(de == null)
 		{
-			de = new De();
+			de = new De(new DeManagerClassic());
 		}
 		return de;
 	}
 
+
+
 	public int getValeur()
 	{
-		int[] valeurs = {1,2,2,3,3,4};
 		int nombre = De.aleatoire.nextInt(5) + 1;
 		return valeurs[nombre];
 	}
---
+++ src/model/de/DeManagerClassic.java
@@ -0,0 +1,13 @@
+package model.de;
+
+import model.*;
+
+public class DeManagerClassic implements DeManager 
+{
+	public int[] face(int faces1)
+	{
+		faces1 = 6;
+		int[] faces = {1,2,2,3,3,4};
+		return faces;
+	}
+}
